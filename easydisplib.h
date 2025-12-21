7a0a4fd29e1d7ceb2e3f62349aa21ee22543b363
Marc <marc.puigpinos@gmail.com>
2025-12-21 22:54:14 +0100

    - Move the img_count variable inside a the function. Avoid ODR violations. - Add the static inline keyword to the function. Avoid ODR violations. - Check EDL_SCREEN pointer is not NULL.

--- easydisplib.h
+++ easydisplib.h
@@ -20,11 +20,15 @@ typedef struct {
     u32 *buffer;
 } EDL_SCREEN;
 
-// Image counter
-size_t img_count = 0;
-
 // Initialize EDL_SCREEN
-int edl_init_screen(EDL_SCREEN *screen, const u32 res_x, const u32 res_y, u32 color) {
+static inline int edl_init_screen(EDL_SCREEN *screen, const u32 res_x, const u32 res_y, u32 color) {
+
+    // Check screen is not null
+    if (screen == NULL) {
+        return EDL_FAILURE;
+    }
+    
+    // Create the screen 
     screen->res_x = res_x;
     screen->res_y = res_y;
     screen->buffer = (u32 *)malloc(res_x * res_y * sizeof(u32));
@@ -34,18 +38,34 @@ int edl_init_screen(EDL_SCREEN *screen, const u32 res_x, const u32 res_y, u32 co
         }
     }    
     return EDL_SUCCESS;
+
 }
 
 // Deallocate EDL_SCREEN
-int edl_dalloc_screen(EDL_SCREEN *screen) {
+static inline int edl_dalloc_screen(EDL_SCREEN *screen) {
+
+    // Check screen is not null
+    if (screen == NULL) {
+        return EDL_FAILURE;
+    }
+
+    // Free memory
     free(screen->buffer);
     screen->res_x = 0;
     screen->res_y = 0;
     return EDL_SUCCESS;
+
 }  
 
 // Show EDL_SCREEN
-int edl_show_screen(const EDL_SCREEN *screen) {
+static inline int edl_show_screen(const EDL_SCREEN *screen) {
+
+    // Check screen is not null
+    if (screen == NULL) {
+        return EDL_FAILURE;
+    }
+   
+    static size_t img_count = 0; // static local var counting the images  
     char filename[255];    // buffer for filename
     char format[20];      // buffer for format string
     
@@ -75,17 +95,25 @@ int edl_show_screen(const EDL_SCREEN *screen) {
     fclose(fp);
     fp = NULL;
     return EDL_SUCCESS;
+    
 }
 
-int edl_clear_screen(EDL_SCREEN *screen, u32 color) {
+static inline int edl_clear_screen(EDL_SCREEN *screen, u32 color) {
+
+    // Check screen is not null
+    if (screen == NULL) {
+        return EDL_FAILURE;
+    }
+       
     // Clear the screen
     for (u32 j=0; j<screen->res_y; j++) {
         for (u32 i=0; i<screen->res_x; i++) {
             screen->buffer[i+j*screen->res_x] = color;
         }
     }
-   
+
     return EDL_SUCCESS;
+    
 }
 
 #endif
